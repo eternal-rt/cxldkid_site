587a2725e4b69b8a91dc26a8973dce3f12d447d2
eternalxyz <39377940+eternal-rt@users.noreply.github.com>
2025-08-03 22:52:29 -0500

    god i keep breaking shi

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,3 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
-
 export default defineConfig({
   base: '/cxldkid_site/',
   plugins: [react()],
